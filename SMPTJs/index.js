48935469fd83e284caf8ca3e81a203396076669c
Dev-Kam <developerkaam@gmail.com>
2021-05-20 19:10:13 -0300

    edicion smpt

--- SMPTJs/index.js
+++ SMPTJs/index.js
@@ -2,12 +2,12 @@ const btn = document.querySelector('button');
 const inputs = document.querySelector('form');
 btn.addEventListener('click', () => {
 	Email.send({
-		//SecureToken: "1e7a7439-29d5-4dbf-ab7e-1bb26765ecfe",
+		SecureToken: "db7c3ecc-ccdb-41b9-9a14-5c14ee6cf334",
 
 		//Host: "smtp.mailtrap.io",
 		//Username: "6158a9ca83ab26",
 		//Password: "9b7f90e7c14048",
-		SecureToken: "eb0ca7be-3287-48cd-a3ca-02403f487133",
+		//SecureToken: "eb0ca7be-3287-48cd-a3ca-02403f487133",
 		To: "kmilodesigner@gmail.com",
 		From: inputs.elements["email"].value,
 		Subject: "Contact Us Query By the Custoner",
