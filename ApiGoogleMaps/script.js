47369608c6622382a9112fd91ac9bf2e5fa9276e
Dev-Kam <developerkaam@gmail.com>
2021-05-24 19:20:41 -0300

    api google

---
+++ ApiGoogleMaps/script.js
@@ -0,0 +1,11 @@
+function iniciarMap(){
+    var coord = {lat:-30.615709,lng: -57.985796};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 8,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
